f238450a40d2960a1c523fd8f0784fc446335ee8
nitinvemuri <80983324+nitinvemuri@users.noreply.github.com>
2021-07-25 02:23:39 -0700

    models and making the frameworks

---
+++ models/Thought.js
@@ -0,0 +1,57 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateformat');
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        match: '/&.{1,280}$/'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    reactions: [reactionSchema]
+}
+);
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+        },
+
+        reactionBody: {
+            type: String,
+            required: true,
+            match: '/&.{0,280}$/'
+        },
+
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    }
+)
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reaction.length
+})
+
+const Thought = model('Thought', thoughtSchema)
+
+module.exports = thought;
---
+++ models/User.js
@@ -0,0 +1,56 @@
+const { Schema, model} = require('mongoose')
+const dateFormat = require('../utils/dateformat')
+
+
+var validateEmail = function(email) {
+    var regex =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return regex.test(email)
+};
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: 'Need a Username Yo',
+            trim: true,
+        },
+
+        email: {
+            type: String,
+            required: 'Email address is required',
+            unique: true,
+            trim: true,
+            validate:[validateEmail, 'Please fill an email address'],
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill an email address']  
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thoughts'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Friends'
+            }
+        ],
+
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false
+        
+    }
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+});
+
+const User = model('User', UserSchema)
